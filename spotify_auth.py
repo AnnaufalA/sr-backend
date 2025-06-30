2f5733801e2b95edc92136388e86655ace3bd47f
Annaufal Arifa <71707026+AnnaufalA@users.noreply.github.com>
2025-06-30 08:32:27 +0700

    Update spotify_auth.py

--- spotify_auth.py
+++ spotify_auth.py
@@ -6,8 +6,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 load_dotenv()
 
 def get_spotify_client():
-    SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
-    SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+    SPOTIPY_CLIENT_ID= os.getenv("SPOTIPY_CLIENT_ID")
+    SPOTIPY_CLIENT_SECRET= os.getenv("SPOTIPY_CLIENT_SECRET")
 
     if not SPOTIPY_CLIENT_ID or not SPOTIPY_CLIENT_SECRET:
         raise Exception("Spotify credentials not found")
